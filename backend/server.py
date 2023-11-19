931a455b8d585ce857dd0272585e8409dd9c462d
Pianissimo7 <edanphi@gmail.com>
2023-11-19 12:39:13 +0200

    standardized the response format

--- backend/server.py
+++ backend/server.py
@@ -5,6 +5,17 @@ app = Flask(__name__)
 # Dummy user data (replace this with your user data or connect to a database)
 users = []
 
+def success_response(data=None, message=None):
+    response = {'success': True}
+    if data is not None:
+        response['data'] = data
+    if message is not None:
+        response['message'] = message
+    return jsonify(response), 200
+
+def error_response(message, status_code):
+    return jsonify({'success': False, 'message': message}), status_code
+
 # Route for handling registration requests
 @app.route('/register', methods=['POST'])
 def register():
@@ -12,12 +23,12 @@ def register():
 
     # Check if the email is already registered
     if any(user['email'] == user_data['email'] for user in users):
-        return jsonify({'success': False, 'message': 'Email is already registered'})
+        return error_response('Email is already registered', 400)
 
     # Add the new user to the list
     users.append(user_data)
 
-    return jsonify({'success': True, 'message': 'Registration successful'})
+    return success_response(message='Registration successful')
 
 # Route for handling login requests
 @app.route('/login', methods=['POST'])
@@ -29,10 +40,10 @@ def login():
 
     if user and user['password'] == login_data['password']:
         # Login successful
-        return jsonify({'success': True, 'message': 'Login successful'})
+        return success_response(message='Login successful')
     else:
         # Invalid email or password
-        return jsonify({'success': False, 'message': 'Invalid email or password'})
+        return error_response('Invalid email or password', 401)
 
 if __name__ == '__main__':
     app.run(debug=True)
