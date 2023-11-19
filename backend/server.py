3ff25051ddda631e62b986170f7cefdfc1c66e26
Pianissimo7 <edanphi@gmail.com>
2023-11-19 17:43:37 +0200

    added a database to store the users, works with login and register

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,30 @@
 from flask import Flask, request, jsonify
+import mysql.connector
 
 app = Flask(__name__)
 
-# Dummy user data (replace this with your user data or connect to a database)
-users = []
+db_config = {
+    'host': '127.0.0.1',
+    'user': 'root',
+    'password': 'Edaniel7',
+    'database': 'db'
+}
+
+def user_exists(email):
+    # Establish a connection to the database
+    connection = mysql.connector.connect(**db_config)
+    cursor = connection.cursor()
+
+    # Check if the user with the given email already exists
+    check_query = "SELECT * FROM users WHERE email = %s"
+    cursor.execute(check_query, (email,))
+    existing_user = cursor.fetchone()
+
+    # Close the cursor and connection
+    cursor.close()
+    connection.close()
+
+    return existing_user is not None
 
 def success_response(data=None, message=None):
     response = {'success': True}
@@ -19,30 +40,52 @@ def error_response(message, status_code):
 # Route for handling registration requests
 @app.route('/register', methods=['POST'])
 def register():
-    user_data = request.get_json()
+    user_data_json = request.get_json()
+    
+    # Establish a connection to the database
+    connection = mysql.connector.connect(**db_config)
+    cursor = connection.cursor()
 
-    # Check if the email is already registered
-    if any(user['email'] == user_data['email'] for user in users):
-        return error_response('Email is already registered', 400)
+    # Check if the user with the given email already exists
+    if user_exists(user_data_json['email']):
+        print(f"User with email {user_data_json['email']} already exists.")
+    else:
+        # Insert the new user into the database
+        insert_query = "INSERT INTO users (name, email, password) VALUES (%s, %s, %s)"
+        user_data = (user_data_json['name'], user_data_json['email'], user_data_json['password'])
+        cursor.execute(insert_query, user_data)
+        connection.commit()
+        print(f"User {user_data_json['name']} registered successfully.")
 
-    # Add the new user to the list
-    users.append(user_data)
+    # Close the cursor and connection
+    cursor.close()
+    connection.close()
 
     return success_response(message='Registration successful')
 
 # Route for handling login requests
 @app.route('/login', methods=['POST'])
 def login():
+    
     login_data = request.get_json()
 
-    # Find the user based on the provided email
-    user = next((user for user in users if user['email'] == login_data['email']), None)
+    # Establish a connection to the database
+    connection = mysql.connector.connect(**db_config)
+    cursor = connection.cursor()
 
-    if user and user['password'] == login_data['password']:
-        # Login successful
+    # Retrieve the user with the given email
+    select_query = "SELECT * FROM users WHERE email = %s"
+    cursor.execute(select_query, (login_data['email'],))
+    user = cursor.fetchone()
+
+    # Close the cursor and connection
+    cursor.close()
+    connection.close()
+
+    # Check if the user exists and the password is correct
+    if user and user[3] == login_data['password']:
         return success_response(message='Login successful')
     else:
-        # Invalid email or password
         return error_response('Invalid email or password', 401)
 
 if __name__ == '__main__':
